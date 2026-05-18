6a3a2cdb54cf84bc3ad3e78b32049f6065e877a0
Xiangpu Shen <xiangpushen@XiangpudeMacBook-Pro.local>
2026-05-18 22:56:10 +0800

    chore: scaffold project with Vite + Vue 3

---
+++ crm-new-project/vite.config.js
@@ -0,0 +1,22 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import { viteMockServe } from 'vite-plugin-mock'
+import path from 'path'
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    viteMockServe({
+      mockPath: 'mock',
+      enable: true
+    })
+  ],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  },
+  server: {
+    port: 3000
+  }
+})
