4f75aad3ea1432e8cb53affeccc1ba895be28aea
Xiangpu Shen <xiangpushen@XiangpudeMacBook-Pro.local>
2026-05-18 23:00:09 +0800

    feat: add App.vue and main.js entry

---
+++ crm-new-project/src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <n-message-provider>
+    <router-view />
+  </n-message-provider>
+</template>
+
+<script setup>
+import { NMessageProvider } from 'naive-ui'
+</script>
+
+<style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
+html, body, #app {
+  height: 100%;
+  width: 100%;
+}
+</style>
